a74505598ec241bd4faa418c69042ee51d1ccc17
Mikael Gois <40373810+MikaelGois@users.noreply.github.com>
2025-07-18 06:02:02 -0300

    Update init.lua

--- lua/smart-runner/init.lua
+++ lua/smart-runner/init.lua
@@ -30,19 +30,17 @@ function M.run()
   vim.cmd("silent! w") -- saves file before run
   local ext = vim.fn.expand("%:e") -- identify file extension
   local pom_path = vim.fn.findfile('pom.xml', '.;') -- find a pom file for maven projects
-
   local command_to_run
 
   -- for maven projects
   if pom_path ~= '' and pom_path ~= nil and ext == 'java' then
     vim.notify("Maven project detected! Running the application...", vim.log.levels.INFO)
-    command_to_run = "mvn compile exec:java"
+    command_to_run = "mvn -f " .. pom_path .. " compile exec:java"
   else
     -- for other files
     local file_dir = vim.fn.expand("%:h")
     local file_name = vim.fn.expand("%:t")
     local file_no_ext = vim.fn.expand("%:t:r")
-
     local config = vim.tbl_deep_extend("force", M.defaults, {})
     local template = config.commands[ext]
 
@@ -50,29 +48,15 @@ function M.run()
       local command = template:gsub("%%f", file_name):gsub("%%c", file_no_ext):gsub("%%e", file_no_ext)
       command_to_run = "cd '" .. file_dir .. "' && " .. command
     else
-      vim.notify("No action defined for extension: " .. ext, vim.log.levels.ERROR)
+      if not os.getenv("SMART_RUNNER_CI") then
+        vim.notify("No action defined for extension: " .. ext, vim.log.levels.ERROR)
+      end
       return
     end
   end
 
   if os.getenv("SMART_RUNNER_CI") then
-    local job_id = vim.fn.jobstart(command_to_run, {
-      stdout_buffered = true,
-      on_stdout = function(_, data)
-        if data then
-          for _, line in ipairs(data) do if line ~= "" then print(line) end end
-        end
-      end,
-      on_stderr = function(_, data)
-        if data then
-          for _, line in ipairs(data) do if line ~= "" then print(line) end end
-        end
-      end,
-    })
-    
-    if job_id and job_id > 0 then
-      vim.fn.jobwait({job_id})
-    end
+    print(command_to_run)
     return
   end
 
